8da024a8e1c19d3bacb9e595b7701403421f9349
functionalloop <ndivya945@gmail.com>
2025-12-02 14:43:27 +0530

    54

--- Video 54/script.js
+++ Video 54/script.js
@@ -1,22 +1,7 @@
-alert("Hello World");
+// alert("hi");
+console.log("hi");
+console.log("whgt its not woring");
 
-console.log("Code is running...")
-console.log("Code is also running...")
-console.log("Code is looking like a wow...")
-
-var a = prompt("Enter your number")
-var isTrue = confirm("Are you sure you want to leave this page and blast your computer ")
-
-if(isTrue){
-    console.log("Computer is blasting")
-}
-
-else{
-    console.log("Computer is not blasting")
-
-}
-console.log("Your number is " + a)
-
-document.title = "Hey I am good"
-
-// document.body.style.backgroundColor = "red"
+var a = prompt(" enter  a numebr");
+ 
+console.log("your name is " + a);
