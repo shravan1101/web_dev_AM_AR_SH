5e0567928ad1ccbd881c4b51ed6307823929dc91
functionalloop <ndivya945@gmail.com>
2025-12-02 14:48:22 +0530

    new_branch

--- Video 54/script.js
+++ Video 54/script.js
@@ -3,5 +3,9 @@ console.log("hi");
 console.log("whgt its not woring");
 
 var a = prompt(" enter  a numebr");
- 
+var ture = prompt(" what you want to do ");
 console.log("your name is " + a);
+
+if (ture == yes) {
+  console.log("what the hell");
+}
