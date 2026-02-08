59a674c2c89e866cfc04b93d8bb87153f5533356
shravan1101 <shravanbhise248@gmail.com>
2026-02-08 22:01:49 +0530

    08-02-12

--- Video 84/script.js
+++ Video 84/script.js
@@ -23,8 +23,23 @@ async function main() {
     .querySelector(".songlist")
     .getElementsByTagName("ul")[0];
   songUL.innerHTML = "";
+
+  // Build each row to match the reference layout: icon + song text on the left, Play Now CTA on the right.
   for (const song of songs) {
-    songUL.innerHTML += `<li>${decodeURIComponent(song)}</li>`;
+    songUL.innerHTML += `<li class="song-item">
+      <div class="song-meta">
+        <img src="music.svg" class="song-icon" alt="music" />
+        <div class="text-container">
+          <div class="song-name">${decodeURIComponent(song)}</div>
+          <div class="song-artist">song Artist</div>
+        </div>
+      </div>
+
+      <div class="playnow">
+        <span>Play Now</span>
+        <img src="play-button.svg" class="invert play-icon" alt="play_button" />
+      </div>
+    </li>`;
   }
 
   // play the first song
