f9d257531c2de78169caacbad2dae216aa9fa8bc
shravan1101 <shravanbhise248@gmail.com>
2026-02-18 01:01:47 +0530

    17-02-26

---
+++ Video 86/export.js
@@ -0,0 +1,10 @@
+export const a = 1; // name exort
+export const b = 1;
+export const c = 1;
+export const d = 1;
+
+let obj;
+export default obj = {
+  x: 5,
+  y: 7,
+};
---
+++ Video 86/import.js
@@ -0,0 +1,9 @@
+// import {a ,b , c ,d } from "./export.js"
+// console.log(a,b,c,d)
+
+// import obj from "./export.js"; // default exort
+
+// console.log(obj);
+
+import shravan from "./export.js";
+console.log(shravan); // you can import the deafult from any name
